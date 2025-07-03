8485b4fc64a24584833f118b94582b649e01d545
SabrinaJB <anitasajimemez@gmail.com>
2025-07-03 11:07:58 -0600

    Update ##Avance 1 Grupo 2##.py

--- ##Avance 1 Grupo 2##.py
+++ ##Avance 1 Grupo 2##.py
@@ -1,34 +1,40 @@
-##Avance 1 Grupo 2##
+from colorama import init, Fore, Back, Style
 
-#Menu del gallinero#
+init()
+
+def main():
+    print(Fore.GREEN + "==Menú del Gallinero==")
+
+##Avance 1 Grupo 2##
 
 opcion = ""
 
 while opcion != "5":
-    print("==Menú del Gallinero==")
+    print(Fore.GREEN + "==Menú del Gallinero==")
     print("1. Incluir datos")
     print("2. Consultar datos")
     print("3. Modificar datos")
     print("4. Borrar datos")
     print("5. Salir")
-    opcion = input("Seleccione una opcion: ")
+    opcion = input(Fore.BLUE + "Seleccione una opcion: ")
 
     if opcion == "1":
-       print("_-Opción en Mantenimiento-_")
+       print(Fore.RED + "_-Opción en Mantenimiento-_")
 
     elif opcion == "2":
-        print("_-Opción en Mantenimiento-_")
+        print(Fore.RED + "_-Opción en Mantenimiento-_")
 
     elif opcion == "3":
-       print("_-Opción en Mantenimiento-_")
+       print(Fore.RED + "_-Opción en Mantenimiento-_")
 
     elif opcion == "4":
-        print("_-Opción en Mantenimiento-_")
+        print(Fore.RED + "_-Opción en Mantenimiento-_")
 
     elif opcion == "5":
-        print("Fin del programa")
+        print(Fore.WHITE + "Fin del programa")
 
     else:
-        print("Opción no válida")
+        print(Fore.LIGHTRED_EX + "Opción no válida")
+        print(Fore.YELLOW + "=====Fin del Programa=====")
 
     
